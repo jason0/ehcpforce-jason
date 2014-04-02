05aa9963914b93434d2a01ba326d3d1203b75106
Jason Brooks <jason@eroi.com>
2014-04-02 15:06:35 -0700

    adding various files... including notes

---
+++ trunk/ehcpforce/ehcp/eroi-install.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+# EHCP Force Edition Pre-Installer Script
+# www.ehcpforce.tk
+# by earnolmartin@gmail.com
+
+###################
+#### FUNCTIONS ####
+###################
+
+function outputInfo(){
+	clear
+	echo "EHCP Force Edition Pre-Install Script"
+	echo "Version 1.0"
+	echo -e "By earnolmartin\n\n"
+	echo "This pre-installation script allows you to select your EHCP Force installation mode."
+	echo -e "For example, you can choose to install EHCP with extra software (Email Anti-Spam, etc).\n"
+}
+
+function generateEHCPPreInstallFile(){
+  logInfoFile="/root/ehcp_info"
+  if [ -e "$logInfoFile" ]; then
+	CurDate=$(date +%Y_%m_%d_%s)
+	mv "$logInfoFile" "/root/ehcp_info_$CurDate"
+  else
+	touch "$logInfoFile"
+  fi
+
+  generatePassword "15"
+  MYSQLROOTPASS="$rPass"
+
+  generatePassword
+  PHPMYADMINPASS="$rPass"
+
+  generatePassword
+  RCUBEPASS="$rPass"
+
+  generatePassword
+  EHCPDBPASS="$rPass"
+
+  generatePassword "20"
+  EHCPADMINPASS="$rPass"
+
+  echo -e "<?php\n\$mysql_root_pass=\"$MYSQLROOTPASS\";\n\$php_myadmin_pass=\"$PHPMYADMINPASS\";\n\$rcube_pass=\"$RCUBEPASS\";\n\$ehcp_mysql_pass=\"$EHCPDBPASS\";\n\$ehcp_admin_password=\"$EHCPADMINPASS\";\n?>" > "install_silently.php"
+
+  echo -e "\nMySQL root user password = $MYSQLROOTPASS"
+  echo "MySQL root user password = $MYSQLROOTPASS" >> "$logInfoFile"
+  echo "PHPMyAdmin MySQL user password = $PHPMYADMINPASS"
+  echo "PHPMyAdmin MySQL user password = $PHPMYADMINPASS" >> "$logInfoFile"
+ # echo "Roundcube MySQL user password = $RCUBEPASS"
+ # echo "Roundcube MySQL user password = $RCUBEPASS" >> "$logInfoFile"
+  echo "EHCP MySQL user password = $EHCPDBPASS"
+  echo "EHCP MySQL user password = $EHCPDBPASS" >> "$logInfoFile"
+  echo "EHCP Admin Password = $EHCPADMINPASS"
+  echo "EHCP Admin Password = $EHCPADMINPASS" >> "$logInfoFile"
+  echo -e "\nThis information has been saved in $logInfoFile for you to reference later!"
+  sleep 5
+}
+
+function generatePassword(){
+  if [ ! -z "$1" ]; then
+    PLENGTH="$1"
+  else
+    PLENGTH="10"
+  fi
+
+  #rPass=$(date +%s | sha256sum | base64 | head -c "$PLENGTH")
+  rPass=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 | head -c "$PLENGTH")
+}
+
+function checkRoot(){
+	# Make sure only root can run our script
+	if [ "$(id -u)" != "0" ]; then
+	   echo "This script must be run as root" 1>&2
+	   exit 1
+	fi
+}
+
+###################
+#### Main Code ####
+###################
+
+# Check for root
+checkRoot
+
+# Get parameters
+for varCheck in "$@"
+do
+    if [ "$varCheck" == "unattended" ]; then
+		preUnattended=1
+    fi
+done
+
+if [ ! -z "$preUnattended" ]; then
+	# They really want this install to be unattended, so run the installer and use default passwords of 1234
+	bash install_main.sh "$@"
+else
+
+	# Echo Info
+	outputInfo
+
+	# Ready to start?
+	echo -n "Install EHCP Force Edition in unattended mode (installs all software without prompts and generates passwords)? [y/n]: "
+	read unattended
+	unattended=$(echo "$unattended" | awk '{print tolower($0)}')
+
+	if [ "$unattended" != "n" ]; then
+		generateEHCPPreInstallFile
+		unattendedMode="unattended"
+	fi
+	echo ""
+	echo -n "Install extra software in addition to EHCP Force Edition (such as Amavis, SpamAssassin, ClamAV)? [y/n]: "
+	read insMode
+	insMode=$(echo "$insMode" | awk '{print tolower($0)}')
+
+	if [ "$insMode" != "n" ]; then
+		extra="extra"
+	else
+		extra="normal"
+	fi
+
+	# Run the main installer
+	echo "bash eroi-install_main.sh $unattendedMode $extra"
+	bash eroi-install_main.sh $unattendedMode $extra
+
+fi
---
+++ trunk/ehcpforce/ehcp/eroi-install_2.php
@@ -0,0 +1,125 @@
+<?php
+error_reporting (E_ALL ^ E_NOTICE);
+//  second part of install.
+//  first part installs mailserver, then, install2 begins, 
+//  i separated these installs because php email function does not work if i re-start php after email install... 
+// install functions in install_lib.php
+
+if($argc>1){
+
+  # Distro and version are always sent
+	
+  # Get distro version number
+  $temp = trim($argv[1]);
+  if(stripos($temp, ".") != FALSE){
+    $version = $temp;
+  }
+  
+  # Distro is needed for Ubuntu only features
+  $distro = strtolower(trim($argv[2]));
+}
+
+for($i=3;$i<=5;$i++){ # accept following arguments in any of position.
+	if($argc>$i) {
+		print "argc:$argc\n\n";
+		switch($argv[$i]) {
+			case 'noapt': # for only simulating install, apt-get installs are still loged onto a file
+				$noapt="noapt";
+				echo "apt-get install disabled due to parameter:noapt \n";
+				break;
+			case 'unattended': # tries to suppress most user dialogs.. good for a quick testing. (tr: hizlica test etmek icin guzel..)
+				$unattended=True;
+				break;
+			case 'light': # the light install, non-cruical parts are omitted. good for a quick testing. (tr: hizlica test etmek icin guzel..)
+				$installmode='light';
+				break;
+			case 'extra': # the extra install, means more components
+				$installmode='extra';
+				break;
+			default:
+				echo __FILE__." dosyasinda bilinmeyen arguman degeri:".$argv[$i];
+				break;
+		}
+
+	}
+}
+echo "Some install parameters for file ".__FILE__.": noapt:($noapt), unattended:".($unattended===True?"exact True":"not True")." installmode:($installmode) \n";
+
+
+include_once('install_lib.php');
+
+// Load preset installation values in install_silently.php if exists:
+if(file_exists("install_silently.php")){
+	include 'install_silently.php';
+}
+
+include_once('install2.1.php');
+
+echo "System is running $version\n";  
+
+echo "\nincluded install2.1.php\nhere are variables transfered:\n";
+echo 
+"
+webdizin:$webdizin
+ip:$ip
+user_name:$user_name
+user_email:$user_email
+hostname:$hostname
+installextrasoftware: $installextrasoftware
+";
+
+/*
+ehcpmysqlpass:$ehcpmysqlpass
+rootpass:$rootpass
+newrootpass:$newrootpass
+ehcpadminpass:$ehcpadminpass
+*/
+
+installsql();
+fail2ban_install();
+
+install_vsftpd_server();
+#infomail('_5_vsftpd install finished');
+
+if(isset($version) && $version != "12.10"){
+  install_nginx_webserver();
+}else{
+  echo "Ubuntu 12.10 is not compatible with nginx due to bugs. Installing apache2 only. Upgrade to Ubuntu 13.04 for this functionality.\n";
+}
+installapacheserver();
+
+# Secure apache by installing some basic Anti-DDoS Modules
+if(!isset($installmode) || $installmode != 'light'){
+	apache_mod_secure_install();
+}
+
+# scandb();  no more need to scan db since ver. 0.29.15
+installfinish();
+
+
+
+$message='';
+exec('ifconfig',$msg);
+exec('ps aux ',$msg);
+foreach($msg as $m) $message.=$m."\n";
+#infomail("_6_install finished.mail from inside php.user:$user_name,$user_email,$yesno",$message);
+$msg="
+your ehcp install finished. if you have questions or need help, please visit www.ehcp.net, ehcp forums or email me to info@ehcp.net
+
+ehcp kurulumunuz tamamlandı. tebrikler. eğer sorularınız varsa ya da yardıma ihtiyacınız varsa, ehcp.net deki forum kısmına soru yazın veya info@ehcp.net adresine eposta gönderin. 
+https://launchpad.net/ehcp  bu adresi de kullanabilirsiniz.
+
+ehcp developer..
+";
+
+if($user_email<>'') mail($user_email,'your ehcp install finished..have fun',$msg,'From: info@ehcp.net');
+
+$realip=getlocalip2();
+if(!$app->isPrivateIp($ip)) $realip.="-realip"; # change subject if this is a server with real ip... 
+$ip2=trim(file_get_contents("http://ehcp.net/diger/myip.php"));
+$message.="\noutside Ip detected:$ip2";
+
+infomail("_7_install finished.$realip.$ip2.mail from inside php.user:$user_name,$user_email,$yesno",$message);
+#infomailusingwget("7_ehcp_install2.php.finished");
+
+?>
